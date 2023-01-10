cdc6c15a0e0051b52c4de7bedbd040313a0da79d
Vinay Kumar Thakur <002avithakur.07@gmail.com>
2023-01-10 20:15:03 +0530

    MasaiSchool

---
+++ masai.js
@@ -0,0 +1,5 @@
+I am here in masai School and living the lifestyle 
+of militery style coding were i am doing study 11 11 6
+which means the whole day 
+here a lot of students are studying and im one of them also
+the students are great and Inteligent
